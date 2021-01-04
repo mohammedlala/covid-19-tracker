1e5373cbe20377298992b6515e6284c806853c1c
mohammedlala <mohammedlala5253@gmail.com>
2021-01-04 17:28:36 +0530

    covid-tracker with firebase

--- src/Map.js
+++ src/Map.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { MapContainer as LeafletMap, TileLayer } from "react-leaflet";
+import { Map as M, TileLayer } from "react-leaflet";
 import "./Map.css";
 import { showDataOnMap } from "./util";
 
 function Map({ countries, casesType, center, zoom }) {
   return (
     <div className="map">
-      <LeafletMap center={center} zoom={zoom}>
+      <M center={center} zoom={zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {showDataOnMap(countries, casesType)}
-      </LeafletMap>
+      </M>
     </div>
   );
 }
