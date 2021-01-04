db4e3887d06064297eab3497dd5f14a226b67842
mohammedlala <mohammedlala5253@gmail.com>
2021-01-04 09:23:20 +0530

    initial commit

--- src/Map.js
+++ src/Map.js
@@ -6,13 +6,13 @@ import { showDataOnMap } from "./util";
 function Map({ countries, casesType, center, zoom }) {
   return (
     <div className='map'>
-      <Mapntainer center={center} zoom={zoom}>
+      <MapContainer center={center} zoom={zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {showDataOnMap(countries, casesType)}
-      </Mapntainer>
+      </MapContainer>
     </div>
   )
 }
